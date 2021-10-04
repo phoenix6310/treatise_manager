77046bfb5678a82c49b81ecb134027ce8a59b66f
pj <phoenix6310@example.com>
2021-10-04 17:13:05 +0800

    'update'

--- vue.config.js
+++ vue.config.js
@@ -1,49 +1,55 @@
 'use strict'
 const path = require('path')
+const NODE_ENV = process.env.NODE_ENV
+let prodPlugins = []
+if (NODE_ENV === 'production') {
+  // 去掉console
+  prodPlugins.push(new UglifyJsPlugin({
+    uglifyOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  }))
+}
 
 function resolve(dir) {
- return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
- publicPath: process.env.VUE_APP_PUBLIC_PATH,
- devServer: {
-  disableHostCheck: true,
-  open: true,
-  port: 9527,
-  host: '192.168.0.105',
-  proxy: {
-   'api': {
-    // target: 'http://172.18.84.42:7006',
-    target: 'http://f3176v6414.zicp.vip',
-    pathRewrite: {
-     '^/api': '',
-     changeOrigin: true
-    },
-    // headers: {
-    //     Host: 'f3176v6414.zicp.vip'
-    // }
-   }
-  }
- },
- productionSourceMap: false,
- configureWebpack: {
-  // provide the app's title in webpack's name field, so that
-  // it can be accessed in index.html to inject the correct title.
-  name: '答辩',
-  resolve: {
-   alias: {
-    '@': resolve('src'),
-   }
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  devServer: {
+    disableHostCheck: true,
+    open: true,
+    port: 9527,
+    host: '192.168.0.105',
+    proxy: {
+      'api': {
+        // target: 'http://172.18.84.42:7006',
+        target: 'http://f3176v6414.zicp.vip',
+        pathRewrite: {
+          '^/api': '',
+          changeOrigin: true
+        },
+        // headers: {
+        //     Host: 'f3176v6414.zicp.vip'
+        // }
+      }
+    }
   },
-  plugins: [
-//    new UglifyJsPlugin({
-//     uglifyOptions: {
-//      compress: {
-//       drop_console: true,
-//      },
-//     },
-//    }),
-  ],
- },
+  productionSourceMap: false,
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: '答辩',
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      }
+    },
+    plugins: [
+      ...prodPlugins,
+    ],
+  },
 }
